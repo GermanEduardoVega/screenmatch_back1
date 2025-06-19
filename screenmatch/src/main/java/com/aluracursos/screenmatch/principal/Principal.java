ff9d7f2d02442c91a0b467f41186c362711db993
GermanEduardoVega <german.vega@alumnos.frm.utn.edu.ar>
2025-06-18 23:58:56 -0300

    uso de DoubleSummaryStatistics para las estadsticas

--- screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/principal/Principal.java
@@ -134,6 +134,7 @@ public class Principal {
         DoubleSummaryStatistics est = episodios.stream()
                 .filter(e -> e.getEvaluacion() > 0.0)
                 .collect(Collectors.summarizingDouble(Episodio::getEvaluacion));
+        System.out.println("\n---ESTADISTICAS---\n " );
         System.out.println("Media de las evaluaciones: " + est.getAverage());
         System.out.println("Episodio Mejor evaluado: " + est.getMax());
         System.out.println("Episodio Peor evaluado: " + est.getMin());
